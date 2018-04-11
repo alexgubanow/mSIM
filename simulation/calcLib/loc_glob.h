8b8824d8b1763d8d48ee7801d4ac889873254e52
Губанов Александр <alexgubanow@outlook.com>
2018-04-11 17:02:12 +0300

    add little console app for testing

--- simulation/calcLib/loc_glob.h
+++ simulation/calcLib/loc_glob.h
@@ -2,22 +2,33 @@
 using namespace System;
 namespace calcLib
 {
-	ref class preob
+	public enum axes
 	{
-		static inline double cosa(double l, double x1, double x2) { return (x2 - x1) / l; };
-		static inline double sina(double l, double y1, double y2) { return (y2 - y1) / l; };
+		x = 0,
+		y = 1,
+		z = 2,
+	};
+	public ref class preob
+	{
 	public:
 		static inline double getL(double x1, double x2, double y1, double y2)
 		{
+			double ds = Math::Sqrt(Math::Pow(x2 - x1, 2) + Math::Pow(y2 - y1, 2));
 			return Math::Sqrt(Math::Pow(x2 - x1, 2) + Math::Pow(y2 - y1, 2));
 		}
-		static void to_loc(double cosa, double x_g, array<double>^ %x_l)
+		static inline double getCosa(double L, double x1, double x2) { return (x2 - x1) / L; };
+		static inline double getSina(double L, double y1, double y2) { return (y2 - y1) / L; };
+		static void to_glob(array<double>^ offset, array<double>^ loc, double sina, double cosa, double L, array<double>^ %glob)
 		{
-			x_l[0] = cosa * x_l[0] + cosa * x_l[1] + cosa * x_l[2];
+			glob[x] = offset[x] +  cosa * loc[x] - sina * loc[y] + 0 * loc[z];
+			glob[y] = offset[y] + cosa * loc[y] + sina * loc[x] + 0 * loc[z];
+			glob[z] = offset[z] + 0 * loc[y] + 0 * loc[y] + 1 * loc[z];
 		}
-		static double to_glob(double cosa, array<double>^ x_l)
+		static void to_loc(array<double>^ offset, array<double>^ glob, double sina, double cosa, double L, array<double>^ %loc)
 		{
-			return cosa * x_l[0] + cosa * x_l[1] + cosa * x_l[2];
+			loc[x] = cosa * (glob[x] - offset[x]) + sina * (glob[y] - offset[y]) + 0 * (glob[z] - offset[z]);
+			loc[y] = cosa * (glob[y] - offset[y]) - sina * (glob[x] - offset[x]) + 0 * (glob[z] - offset[z]);
+			loc[z] = 0 * (glob[x] - offset[x]) + 0 * (glob[y] - offset[y]) + 1 * (glob[z] - offset[z]);
 		}
 	};
 }
---
+++ simulation/debugApp/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using calcLib;
+
+namespace debugApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isWork = true;
+            do
+            {
+                Console.WriteLine("To local or global? [l/g]");
+                bool isToLoc = Console.ReadKey(false).Key == ConsoleKey.L ? true : false;
+                Console.WriteLine();
+                if (isToLoc)
+                {
+                    double[] initState = new double[6];
+                    double[] glob = new double[3];
+                    double[] offset = new double[3];
+                    initState[0] = 25;
+                    initState[1] = 22.5;
+                    initState[2] = 0;
+                    initState[3] = 100;
+                    initState[4] = 47.5;
+                    initState[5] = 0;
+                    glob[0] = 35;
+                    glob[1] = 40;
+                    offset[0] = 25;
+                    offset[1] = 22.5;
+                    //Console.WriteLine("Enter initial X1 global");
+                    //double.TryParse(Console.ReadLine().Replace(',', '.'), NumberStyles.Any, CultureInfo.InvariantCulture, out initState[0]);
+                    //Console.WriteLine("Enter initial Y1 global");
+                    //double.TryParse(Console.ReadLine().Replace(',', '.'), NumberStyles.Any, CultureInfo.InvariantCulture, out initState[1]);
+                    //Console.WriteLine("Enter initial X2 global");
+                    //double.TryParse(Console.ReadLine().Replace(',', '.'), NumberStyles.Any, CultureInfo.InvariantCulture, out initState[3]);
+                    //Console.WriteLine("Enter initial Y2 global");
+                    //double.TryParse(Console.ReadLine().Replace(',', '.'), NumberStyles.Any, CultureInfo.InvariantCulture, out initState[4]);
+                    //Console.WriteLine("Enter new X1 global");
+                    //double.TryParse(Console.ReadLine().Replace(',', '.'), NumberStyles.Any, CultureInfo.InvariantCulture, out glob[0]);
+                    //Console.WriteLine("Enter new Y1 global");
+                    //double.TryParse(Console.ReadLine().Replace(',', '.'), NumberStyles.Any, CultureInfo.InvariantCulture, out glob[1]);
+                    double[] loc = new double[6];
+                    double L = preob.getL(initState[0], initState[3], initState[1], initState[4]);
+                    Console.WriteLine("L= {0:F5}", L);
+                    double sina = preob.getSina(L, initState[1], initState[4]);
+                    Console.WriteLine("sina= {0:F5}", sina);
+                    double cosa = preob.getCosa(L, initState[0], initState[3]);
+                    Console.WriteLine("cosa= {0:F5}", cosa);
+                    preob.to_loc(offset, glob, sina, cosa, L, ref loc);
+                    Console.WriteLine("local coords:");
+                    Console.WriteLine("x1= {0:F5}", loc[0]);
+                    Console.WriteLine("y1= {0:F5}", loc[1]);
+                    Console.WriteLine("Transformation is done");
+                }
+                Console.WriteLine("again? [y/n]");
+                isWork = Console.ReadKey(false).Key == ConsoleKey.Y ? true : false;
+                Console.WriteLine();
+            } while (isWork);
+            Console.WriteLine("Enter any key to exit");
+            Console.ReadKey();
+        }
+    }
+}
