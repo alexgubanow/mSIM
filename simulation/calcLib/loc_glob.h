b7f1560fbd16188616afadda4f03f1ae924bc8c5
Губанов Александр <alexgubanow@outlook.com>
2018-04-11 15:30:53 +0300

    add calc func for sina and cosa

--- simulation/calcLib/loc_glob.h
+++ simulation/calcLib/loc_glob.h
@@ -4,8 +4,13 @@ namespace calcLib
 {
 	ref class preob
 	{
-		inline double cosa(double l, double x_g1, double x_g2) { return (x_g2 - x_g1) / l; };
+		static inline double cosa(double l, double x1, double x2) { return (x2 - x1) / l; };
+		static inline double sina(double l, double y1, double y2) { return (y2 - y1) / l; };
 	public:
+		static inline double getL(double x1, double x2, double y1, double y2)
+		{
+			return Math::Sqrt(Math::Pow(x2 - x1, 2) + Math::Pow(y2 - y1, 2));
+		}
 		static void to_loc(double cosa, double x_g, array<double>^ %x_l)
 		{
 			x_l[0] = cosa * x_l[0] + cosa * x_l[1] + cosa * x_l[2];
